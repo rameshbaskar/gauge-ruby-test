fc44fc088692019c2e393f35c80882f182d27ebf
Ramesh Baskarasubramanian <ramesh.baskarasubramanian@live.com>
2020-03-13 13:40:04 +0530

    Made the WordManager module methods non-static

--- step_implementations/lib/utils/word_manager.rb
+++ step_implementations/lib/utils/word_manager.rb
@@ -1,14 +1,13 @@
 module WordManager
-
-  def self.init_vowels(list_of_vowels)
+  def init_vowels(list_of_vowels)
     @list_of_vowels = list_of_vowels
   end
 
-  def self.print_vowel_count(word, count)
+  def print_vowel_count(word, count)
     puts "Word #{word} has #{count} vowels."
   end
 
-  def self.parse_word_table(table)
+  def parse_word_table(table)
     table.rows.each do |row|
       word = row[0]
       vowel_count = row[1]
--- step_implementations/parse_words_steps.rb
+++ step_implementations/parse_words_steps.rb
@@ -1,13 +1,14 @@
 require_relative 'base_steps.rb'
+include WordManager
 
 step 'Vowels in English language are <vowels>' do |vowels|
-  WordManager.init_vowels(vowels)
+  init_vowels(vowels)
 end
 
 step 'The word <word> has <vowel_count> vowels' do |word, vowel_count|
-  WordManager.print_vowel_count(word, vowel_count)
+  print_vowel_count(word, vowel_count)
 end
 
 step 'Almost all words have vowels <table>' do |table|
-  WordManager.parse_word_table(table)
+  parse_word_table(table)
 end
